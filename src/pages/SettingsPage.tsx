8cc745c6d7b497cca5557eb623d380a897fcf3e8
Haranana <hubert.osmalek@op.pl>
2025-09-15 11:44:24 +0200

    updated settings visuals

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -5,10 +5,6 @@ import type {AliasesFields} from '../features/SimulationAliases/simulationAliase
 import {CheckIcon} from "@heroicons/react/24/solid"
 import { useRef } from 'react';
 
-
-
-
-
 export default  function SettingsPage() {
 
     const { sep1, sep2, left, stay, right, setAll, setField} = useSimulationAliases()
@@ -36,6 +32,9 @@ export default  function SettingsPage() {
         let rightRefValue = rightRef.current?.value;
         rightRefValue = rightRefValue === undefined? right : rightRefValue as string;
 
+
+        sep2RefValue = sep1RefValue // ! currently API only accepts 1 separator, change when API is updated
+
         const newValues : AliasesFields = {
             sep1: sep1RefValue,
             sep2: sep2RefValue,
@@ -49,17 +48,44 @@ export default  function SettingsPage() {
         console.log(sep1);
     }
 
-
     return(
-        <div className="page">
-            <div className='simulation-aliases'>
-                <input ref={sep1Ref} className="settings-text-field sep1-text-field" type="text" name="sep1-alias" id="sep1-alias" defaultValue={sep1}/>
-                <input ref={sep2Ref} className="settings-text-field sep2-text-field" type="text" name="sep2-alias" id="sep2-alias" defaultValue={sep2}/>
-                <input ref={leftRef} className="settings-text-field left-text-field" type="text" name="left-alias" id="left-alias" defaultValue={left}/>
-                <input ref={stayRef} className="settings-text-field stay-text-field" type="text" name="stay-alias" id="stay-alias" defaultValue={stay}/>
-                <input ref={rightRef} className="settings-text-field right-text-field" type="text" name="right-alias" id="right-alias" defaultValue={right}/>
-                <button className='save-simulation-aliases' onClick={saveSimulationAliases} ><CheckIcon></CheckIcon></button>
+        <div>
+            <div className='settings-segment'>
+                <div className='settings-segment-text'>
+                    <p className='settings-segment-text-title'>Simulation Aliases</p>
+                    <p className='settings-segment-text-description'>Choose what symbol or word should indicate separator, move left, move right and stay in your Turing machine program.</p>
+                </div>
+                <div className='simulation-aliases'>
+                    <div className='settings-text-field-row'>
+                        <label className="settings-text-field-label" htmlFor='sep1-alias' >Separator:</label>
+                        <input ref={sep1Ref} className="settings-text-field sep1-text-field" type="text" name="sep1-alias" maxLength={10} id="sep1-alias" defaultValue={sep1}/>
+                    </div>
+                    
+                    <div className='settings-text-field-row'>
+                        <label className="settings-text-field-label" htmlFor='sep2-alias'>Separator 2:</label>
+                        <input ref={sep2Ref} className="settings-text-field sep2-text-field" type="text" name="sep2-alias" maxLength={10}  id="sep2-alias" defaultValue={sep2}/>
+                    </div>
+
+                    <div className='settings-text-field-row'>
+                        <label className="settings-text-field-label" htmlFor='left-alias'>Move head left:</label>
+                        <input ref={leftRef} className="settings-text-field left-text-field" type="text" name="left-alias" maxLength={10}  id="left-alias" defaultValue={left}/>
+                            
+                    </div>
+                    <div className='settings-text-field-row'>
+                        <label className="settings-text-field-label" htmlFor='stay-alias'>Don't Move head:</label>
+                        <input ref={stayRef} className="settings-text-field stay-text-field" type="text" name="stay-alias" maxLength={10}  id="stay-alias" defaultValue={stay}/>
+                            
+                    </div>
+                    <div className='settings-text-field-row'>
+                        <label className="settings-text-field-label" htmlFor='right-alias'>Move head right:</label>
+                        <input ref={rightRef} className="settings-text-field right-text-field" type="text" name="right-alias" maxLength={10}  id="right-alias" defaultValue={right}/>
+                            
+                    </div>
+                    <button className='save-simulation-aliases' onClick={saveSimulationAliases} >Save</button>
+                </div>
             </div>
+
+            <hr className='line-separator'></hr>
         </div>  
     );
 }
