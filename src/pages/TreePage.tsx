10ca8f92f00dd10c293d1a0e331f5ed7a8d665a8
Haranana <hubert.osmalek@op.pl>
2025-11-27 12:58:26 +0100

    updated treePage to correctly update nodes and edges status

--- src/pages/TreePage.tsx
+++ src/pages/TreePage.tsx
@@ -1,10 +1,10 @@
 import "./tree.css"
 
 import '@xyflow/react/dist/style.css';
-import {useEffect, useState } from "react";
+import {useEffect, useRef, useState } from "react";
 import { ReactFlow,Background,useNodesState,useEdgesState,type Node,type Edge, type NodeProps, type NodeTypes, Handle, Position} from "@xyflow/react";
 import { type SimulationNode , type SimulationNodeMap } from '../features/Tape/simulationTypes';
-import ELK from "elkjs/lib/elk.bundled.js";
+import ELK, {type ElkNode } from "elkjs/lib/elk.bundled.js";
 import { useSimulationData } from '../features/GlobalData/simulationData';
 import { useSpecialStates } from '../features/GlobalData/specialStates';
 import { NdSimulation } from '../features/Tape/Simulation';
@@ -40,7 +40,9 @@ export default function TreePage(){
   const {simulationData, setSimulationPath, simulationPath} = useSimulationData();
   const [simulation, setSimulation] = useState<NdSimulation | null>(null);
   const [selectedNodesAndEdges , setSelectedNodesAndEdges] = useState<SelectedNodesAndEdges>({nodes: new Map<number, boolean>() , edges: new Map<number, boolean>()})
-  const [graphData, setGraphData ] = useState<GraphData>({children: [], edges: []});
+  const graphData = useRef<GraphData>({children: [], edges: []});
+  const [elkGraph, setElkGraph] = useState<ElkNode>();
+
   //call at start of the component (just after simulationData is loaded)
   //create simulation based on zustand data
   //convert simulation data to graphData, digestable by elk
@@ -54,10 +56,12 @@ export default function TreePage(){
     setSimulation(newSimulation);
 
     const startingGraphData = simulationToGraphChildren(newSimulation);
-    setGraphData(startingGraphData);
+    console.log("init graphData: ", startingGraphData);
+    //setGraphData(startingGraphData);
+    graphData.current = startingGraphData;
     graphDataToSelectedNodesAndEdges(startingGraphData);
 
-      const elkGraph = {
+      const newElkGraph = {
         id: "root",
         layoutOptions: {
             "elk.algorithm": "mrtree",
@@ -77,10 +81,11 @@ export default function TreePage(){
             targets: [e.targetId.toString()],
         })),
       };
+      setElkGraph(newElkGraph)
 
     //create layout and fill react flow page
     const getLayout = async () => {
-      const res = await elk.layout(elkGraph);
+      const res = await elk.layout(newElkGraph);
 
       const elkNodes: RfNode[] =
         res.children?.map(node => ({
@@ -101,7 +106,7 @@ export default function TreePage(){
         source: edge.sources[0],
         target: edge.targets[0],
         type: "straight",
-        className: isEdgeSelected(Number(edge.id))? "SimulationEdge SelectedNode" : "SimulationEdge",
+        className: isEdgeSelected(Number(edge.id))? "SimulationEdge SelectedEdge" : "SimulationEdge",
       })) ?? [];
 
       setNodes(elkNodes);
@@ -111,6 +116,21 @@ export default function TreePage(){
     getLayout();
 
   }, [setNodes, setEdges, simulationData]);
+
+  function updateNodesAndEdgedClasses(selectedNodes: Map<number, boolean> , selectedEdges: Map<number, boolean>){
+    console.log("updateNodesAndEdgedClasses,  selectedNodes: ", selectedNodes, "\nselectedEdges: ", selectedEdges )
+        setNodes(prev => prev.map(n => (
+          { 
+            ...n,
+            className: selectedNodes.get(Number(n.id)) === true ? "SelectedNode" : "",
+          })));
+
+    setEdges(eds => eds.map(e => (
+      {
+        ...e,
+        className: selectedEdges.get(Number(e.id)) === true? "SimulationEdge SelectedEdge": "SimulationEdge",
+      })));  
+  }
 
   //NdSimulation to GraphData to be used by elk to create graph layout
   function simulationToGraphChildren(currentSimulation: NdSimulation): GraphData{
@@ -222,18 +242,22 @@ export default function TreePage(){
         return sIn && tIn;
       };
 
-      graphData.children.forEach((node)=>{
+      console.log("graphData: ", graphData);
+
+      graphData.current.children.forEach((node)=>{
         if(nodeInPath(node.id)) nodes.set(node.id, true);
         else nodes.set(node.id, false);
       });
 
-      graphData.edges.forEach((edge)=>{
+      graphData.current.edges.forEach((edge)=>{
         if(edgeInPath(edge.sourceId, edge.targetId)) edges.set(edge.id, true);
         else edges.set(edge.id, false);
       });
 
+      console.log("nodes and edges: ", nodes, " | ", edges);
       setSimulationPath(newPath)
       setSelectedNodesAndEdges({nodes: nodes, edges: edges});
+      updateNodesAndEdgedClasses(nodes, edges);
       console.log("current path (local|zustand): ", newPath , " | " ,simulationPath);
     };
 
