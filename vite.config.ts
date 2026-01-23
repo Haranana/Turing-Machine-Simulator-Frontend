941c7f6ae2b18c6b1979acd061d303b909631034
Haranana <hubert.osmalek@op.pl>
2026-01-23 15:57:26 +0100

    changed config files to in order to fix deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 import path from "path";
 import { fileURLToPath } from "url";
+import { vitePrerenderPlugin } from "vite-prerender-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
-
 const pathResolve = (p: string) => path.resolve(process.cwd(), p);
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: pathResolve("index.html"),
-        app: pathResolve("app.html"),
+export default defineConfig(() => {
+  const prerenderEnabled = process.env.PRERENDER !== "false";
+
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          main: pathResolve("index.html"),
+          app: pathResolve("app.html"),
+        },
       },
     },
-  },
-  plugins: [
-    react(),
-    tsconfigPaths({ projects: ["./tsconfig.app.json"] }),
-    vitePrerenderPlugin({
-      renderTarget: "#root",
-      prerenderScript: path.resolve(__dirname, "src/prerender.tsx"),
-      additionalPrerenderRoutes: ["/"], 
-    }),
-  ],
+    plugins: [
+      react(),
+      tsconfigPaths({ projects: ["./tsconfig.app.json"] }),
+      prerenderEnabled &&
+        vitePrerenderPlugin({
+          renderTarget: "#root",
+          prerenderScript: path.resolve(__dirname, "src/prerender.tsx"),
+          additionalPrerenderRoutes: ["/"],
+        }),
+    ].filter(Boolean),
+  };
 });
